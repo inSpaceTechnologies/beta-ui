bf131bd0f952b75d03afcdd2d627da45d2de209e
sgerami <sg@inspace.tv>
2018-05-02 18:26:22 +0100

    Basic webpack build.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World.');
+  res.send('<script src="bundle.js"></script>Hello World.');
 });
 
 app.listen(3001, () => {
